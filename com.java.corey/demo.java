64411a3ca5d3fa744df1bb72739d7f761e1e3f3b
corey <1595162370@qq.com>
2020-06-19 08:33:27 +0800

    提交新增文件

---
+++ com.java.corey/demo.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: corey
+ * @Date: 2020/6/18 11:13
+ * @Description:
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println(Math.round(-1.5));
+    }
+}
